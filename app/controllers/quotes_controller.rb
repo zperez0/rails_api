c1bc63e4c4b3d76911e0e86a293ae7ab3deba2fa
Yazmin Perez <zoeperez333@gmail.com>
2022-06-21 12:42:35 -0700

    setup quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,36 @@
 class QuotesController < ApplicationController
 
   def index
-    @quotes = {"quotation": "The secret of getting ahead is getting started."}
+    @quotes = Quote.all
     json_response(@quotes)
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+    json_response(@quote)
+  end
+
+  def create
+    @quote = Quote.create(quote_params)
+    json_response(@quote)
+  end
+
+  def update
+    @quote = Quote.find(params[:id])
+    @quote.update(quote_params)
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
   end
 
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def quote_params
+    params.permit(:author, :content)
   end
 end
