a0853846bff0abf6df50343c2d8f2128c4930056
Yazmin Perez <zoeperez333@gmail.com>
2022-06-22 15:16:53 -0700

    [WIP] add Response module and set up exception handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::API
   include Response
 
-  rescue_from ActiveRecord::RecordNotFound do |exception|
+  # rescue_from ActiveRecord::RecordNotFound do |exception|
+  #   json_response({ message: exception.message }, :not_found)
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def render_not_found_response(exception)
     json_response({ message: exception.message }, :not_found)
+  end
+
+  def render_unprocessable_entity_response(exception)
+    json_response({ message: exception.message }, :unprocessable_entity)
   end
 end
