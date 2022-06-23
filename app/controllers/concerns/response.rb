984431997384db63cb331c356ced5ed386cec4ea
Yazmin Perez <zoeperez333@gmail.com>
2022-06-22 21:16:07 -0700

    [WIP] add not_found and unprocessable_entity to module

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -26,3 +26,26 @@ end
 #     end
 #   end
 # end
+
+# require "active_support/concern"
+
+# module Response
+#   extended ActiveSupport::Concern
+
+#   included do
+#     def json_response(object, status = :ok)
+#       render json: object, status: status
+
+#       rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+#       rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+#       def render_unprocessable_entity_response(exception)
+#         json_response({ message: exception.message }, :unprocessable_entity)
+#       end
+
+#       def render_not_found_response(exception)
+#         json_response({ message: exception.message }, :not_found)
+#       end
+#     end
+#   end
+# end
