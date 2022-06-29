c657fe18cc5f9b22832eddf984db2d236b01ceae
Yazmin Perez <zoeperez333@gmail.com>
2022-06-29 11:31:46 -0700

    add namespace to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'quotes#index'
-  resources :quotes
+  # root to: 'quotes#index'
+  namespace :api do
+    namespace :v1 do
+      resources :quotes
+    end
+  end
 end
